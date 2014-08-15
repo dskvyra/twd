cc39a3955facfbb1463d612b2aceb09e2286ddec
Dmitriy Skvira <d_skvira@wargaming.net>
2014-08-15 16:18:13 +0300

    Add template & make main view use it (4.1.3)

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -1,7 +1,10 @@
-from django.http import HttpResponse
+from django.template import  RequestContext
+from django.shortcuts import render_to_response
 
 def index(request):
-    return HttpResponse("Rango says hello world! <a href='/rango/about'>About</a>")
+    context = RequestContext(request)
+    context_dict = {'boldmessage': 'I\'m bold font from context',}
+    return render_to_response('rango/index.html', context_dict, context)
 
 def about(request):
     return HttpResponse("Rango says: Here is the about page.  <a href='/rango'>Main page</a>")
