2bf8d27945b5ee11d22497e0a5230e44325d20c3
Dmitriy Skvira <d_skvira@wargaming.net>
2014-08-15 17:11:06 +0300

    Add "about" page with own tamplate & view

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -4,7 +4,10 @@ from django.shortcuts import render_to_response
 def index(request):
     context = RequestContext(request)
     context_dict = {'boldmessage': 'I\'m bold font from context',}
+
     return render_to_response('rango/index.html', context_dict, context)
 
 def about(request):
-    return HttpResponse("Rango says: Here is the about page.  <a href='/rango'>Main page</a>")
+    context_dict = {'boldmessage': 'Hello, I\'m Rango and I\'m green'}
+
+    return render_to_response('rango/about.html', context_dict)
