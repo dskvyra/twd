d43dc89dd057220ce79fd7a1fb934a2a72490033
Dmitriy Skvira <d_skvira@wargaming.net>
2014-08-15 18:34:47 +0300

    Fix plural spelling in Category model

--- tango_with_django_project/rango/models.py
+++ tango_with_django_project/rango/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 
 class Category(models.Model):
+    class Meta:
+        verbose_name_plural = 'Categories'
+        
     name = models.CharField(max_length=128, unique=True)
 
     def __unicode__(self):
