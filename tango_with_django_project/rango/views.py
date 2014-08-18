59771f48beeb9199b9321628489d87256ab8d069
Dmitriy Skvira <d_skvira@wargaming.net>
2014-08-18 11:42:10 +0300

    Modify rango's views.index & template, now we have top5 by like categoies on main page

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -1,9 +1,11 @@
 from django.template import  RequestContext
 from django.shortcuts import render_to_response
+from rango.models import Category, Page
 
 def index(request):
     context = RequestContext(request)
-    context_dict = {'boldmessage': 'I\'m bold font from context',}
+    category_list = Category.objects.order_by('-likes')[:5]
+    context_dict = {'categories': category_list,}
 
     return render_to_response('rango/index.html', context_dict, context)
 
