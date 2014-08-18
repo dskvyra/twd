795322fcd7e6c99f6efc2bd33dd43a67e8a7a390
Dmitriy Skvira <d_skvira@wargaming.net>
2014-08-18 19:19:56 +0300

    Add UserForm, UserProfileForm & registering feature (8.4.1 - 8.5]

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -1,8 +1,8 @@
 from django.template import  RequestContext
 from django.shortcuts import render_to_response
 from rango.models import Category, Page
-from rango.forms import CategoryForm
-from rango.forms import PageForm
+from rango.forms import CategoryForm, PageForm, UserForm, UserProfileForm
+
 
 TOP_AMOUNT = 5
 
@@ -97,3 +97,42 @@ def add_page(request, category_name_url):
              'category_name': category_name,
              'form': form,},
             context)
+
+def register(request):
+    context = RequestContext(request)
+
+    registered = False
+
+    if request.method == 'POST':
+        user_form = UserForm(data=request.POST)
+        profile_form = UserProfileForm(data=request.POST)
+
+        if user_form.is_valid() and profile_form.is_valid():
+            user = user_form.save()
+            user.set_password(user.password)
+            user.save()
+
+            profile = profile_form.save(commit=False)
+            profile.user = user
+
+            if 'picture' in request.FILES:
+                profile.picture = request.FILES['picture']
+
+            profile.save()
+
+            registered = True
+
+        else:
+            print user_form.errors, profile_form.errors
+
+    else:
+        user_form = UserForm()
+        profile_form = UserProfileForm()
+
+    return render_to_response(
+        'rango/register.html',
+        {'user_form': user_form,
+         'profile_form': profile_form,
+         'registered': registered,},
+        context
+    )
