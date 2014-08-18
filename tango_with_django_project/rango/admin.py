d71c6b56da2f1b915c1fc82d4c9746fb1c5257b0
Dmitriy Skvira <d_skvira@wargaming.net>
2014-08-18 11:12:11 +0300

    Add views & likes attrs to Category, customizing admin's panel

--- tango_with_django_project/rango/admin.py
+++ tango_with_django_project/rango/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
 from rango.models import Page, Category
 
-admin.site.register(Category)
-admin.site.register(Page)
+class PageAdmin(admin.ModelAdmin):
+    list_display = ('category', 'title', 'url', 'views')
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'views', 'likes')
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Page, PageAdmin)
--- tango_with_django_project/rango/models.py
+++ tango_with_django_project/rango/models.py
@@ -3,8 +3,10 @@ from django.db import models
 class Category(models.Model):
     class Meta:
         verbose_name_plural = 'Categories'
-        
+
     name = models.CharField(max_length=128, unique=True)
+    views = models.IntegerField(default=0)
+    likes = models.IntegerField(default=0)
 
     def __unicode__(self):
         return self.name
@@ -17,3 +19,4 @@ class Page(models.Model):
 
     def __unicode__(self):
         return self.title
+
