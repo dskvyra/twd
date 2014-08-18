db5b981616de9f7771b1627e3648c72ac7a33772
Dmitriy Skvira <d_skvira@wargaming.net>
2014-08-18 14:10:25 +0300

    Add categories template and mapping (whole 6 chapter).

--- tango_with_django_project/populate_rango.py
+++ tango_with_django_project/populate_rango.py
@@ -5,39 +5,47 @@ def populate():
 
     add_page(cat=python_cat,
         title='Official Python Tutorial',
-        url='http://docs.python.org/2/tutorial/')
+        url='http://docs.python.org/2/tutorial/',
+        views=344)
 
     add_page(cat=python_cat,
         title="How to Think like a Computer Scientist",
-        url="http://www.greenteapress.com/thinkpython/")
+        url="http://www.greenteapress.com/thinkpython/",
+        views=12)
 
     add_page(cat=python_cat,
         title="Learn Python in 10 Minutes",
-        url="http://www.korokithakis.net/tutorials/python/")
+        url="http://www.korokithakis.net/tutorials/python/",
+        views=700)
 
-    django_cat = add_cat("Django", views=128, likes=64)
+    django_cat = add_cat("Django", views=64, likes=32)
 
     add_page(cat=django_cat,
         title="Official Django Tutorial",
-        url="https://docs.djangoproject.com/en/1.5/intro/tutorial01/")
+        url="https://docs.djangoproject.com/en/1.5/intro/tutorial01/",
+        views=16)
 
     add_page(cat=django_cat,
         title="Django Rocks",
-        url="http://www.djangorocks.com/")
+        url="http://www.djangorocks.com/",
+        views=87)
 
     add_page(cat=django_cat,
         title="How to Tango with Django",
-        url="http://www.tangowithdjango.com/")
+        url="http://www.tangowithdjango.com/",
+        views=90)
 
-    frame_cat = add_cat("Other Frameworks")
+    frame_cat = add_cat("Other Frameworks", views=32, likes=16)
 
     add_page(cat=frame_cat,
         title="Bottle",
-        url="http://bottlepy.org/docs/dev/")
+        url="http://bottlepy.org/docs/dev/",
+        views=500)
 
     add_page(cat=frame_cat,
         title="Flask",
-        url="http://flask.pocoo.org")
+        url="http://flask.pocoo.org",
+        views=8)
 
     for c in Category.objects.all():
         for p in Page.objects.filter(category=c):
--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -2,10 +2,26 @@ from django.template import  RequestContext
 from django.shortcuts import render_to_response
 from rango.models import Category, Page
 
+TOP_AMOUNT = 5
+
+def decode_url(url):
+    """ Takes url string, returns title string with spaces instead underlines """
+    return url.replace('_', ' ')
+
+def encode_url(title):
+    """ Takes title string, swaps spaces by underlines and returns url string """
+    return title.replace(' ', '_')
+
 def index(request):
     context = RequestContext(request)
-    category_list = Category.objects.order_by('-likes')[:5]
-    context_dict = {'categories': category_list,}
+    category_list = Category.objects.order_by('-likes')[:TOP_AMOUNT]
+    page_top3_list = Page.objects.order_by('-views')[:TOP_AMOUNT]
+    context_dict = {'categories': category_list,
+                    'pages': page_top3_list,
+                    }
+
+    for category in category_list:
+        category.url = encode_url(category.name)
 
     return render_to_response('rango/index.html', context_dict, context)
 
@@ -13,3 +29,18 @@ def about(request):
     context_dict = {'boldmessage': 'Hello, I\'m Rango and I\'m green'}
 
     return render_to_response('rango/about.html', context_dict)
+
+def category(request, category_name_url):
+    context = RequestContext(request)
+    category_name = decode_url(category_name_url)
+    context_dict = {'category_name': category_name,}
+
+    try:
+        category = Category.objects.get(name=category_name)
+        pages = Page.objects.filter(category=category)
+        context_dict['pages'] = pages
+        context_dict['category'] = category
+    except Category.DoesNotExist:
+        pass
+
+    return render_to_response('rango/category.html', context_dict, context)
